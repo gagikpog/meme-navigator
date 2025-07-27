707567788516ce1eacfd7757dfc9c337b747fbb0
Gagik <gagikpog@gmail.com>
2025-07-27 22:31:30 +0500

    add images cache

--- front/src/components/ImageWithAuth.js
+++ front/src/components/ImageWithAuth.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { authFetch } from '../utils/authFetch';
+const cache = new Map();
 
 const ImageWithAuth = ({ src, alt = '', className = '' }) => {
   const [blobUrl, setBlobUrl] = useState(null);
@@ -7,11 +8,17 @@ const ImageWithAuth = ({ src, alt = '', className = '' }) => {
   useEffect(() => {
     const loadImage = async () => {
       try {
+        if (cache.get(src)) {
+          setBlobUrl(cache.get(src));
+          return;
+        }
+
         const res = await authFetch(src);
         if (!res.ok) throw new Error('Ошибка загрузки изображения');
         const blob = await res.blob();
         const url = URL.createObjectURL(blob);
         setBlobUrl(url);
+        cache.set(src, url);
       } catch (err) {
         console.error('Ошибка загрузки изображения:', err);
       }
