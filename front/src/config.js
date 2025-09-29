07530e30530f9ebf213da22d7dc6512c8bde6cab
Gagik <gagikpog@gmail.com>
2025-09-29 21:10:37 +0500

    Add roadmap for meme features and enhance debug configuration
    - Introduced a new roadmap document outlining proposed features for meme functionality, including public approval, a TODO list for user submissions, ratings, comments, and photo upload previews.
    - Updated the configuration to allow toggling debug mode via local storage, enhancing development flexibility.

--- front/src/config.js
+++ front/src/config.js
@@ -1,3 +1,10 @@
 export const isDebug = !(window.localStorage.getItem('isDebug') !== 'on');
 export const API_URL = isDebug ? 'http://localhost:8003/meme' : 'https://gagikpog-api.ru/meme';
 export const IMAGE_URL = '/images';
+
+if (typeof window !== 'undefined') {
+    window.setDebug = (value = true) => {
+        window.localStorage.setItem('isDebug', value ? 'on' : 'off');
+        document.location.reload();
+    }
+}
